764691f2db92051a4f4f4eae2a2a995149ab6ea0
247033 Wojciech Mokwiński <mokwinskiwojciech0@gmail.com>
2023-12-01 13:14:26 +0100

    Task5 setup

---
+++ task5/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <div className="theory">
+        <h1 className="section-heading">
+          Theoretical Part
+        </h1>
+      </div>
+
+      <div className="screenshots">
+        <h1 className="section-heading">
+          Practical Part
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
